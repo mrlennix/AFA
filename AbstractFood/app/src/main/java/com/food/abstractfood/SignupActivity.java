843dc48e86ab2b38bf4c9d8bbc18a57cb14472e5
mistereabate <mistretef@yahoo.com>
2016-04-13 10:57:50 -0500

    testing password field in signup

--- AbstractFood/app/src/main/java/com/food/abstractfood/SignupActivity.java
+++ AbstractFood/app/src/main/java/com/food/abstractfood/SignupActivity.java
@@ -2,12 +2,23 @@ package com.food.abstractfood;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
+    EditText passwordF;
+    EditText verifyPasswordF;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        passwordF=(EditText) findViewById(R.id.passwordtxt);
+
+        passwordF.setText("Password", TextView.BufferType.EDITABLE);
     }
+
+
 }
