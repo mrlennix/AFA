7ef11f9c28becb3e1441e56e12725cf348747032
mistereabate <mistretef@yahoo.com>
2016-04-13 12:31:30 -0500

    minor modifications to sign up

--- AbstractFood/app/src/main/java/com/food/abstractfood/SignupActivity.java
+++ AbstractFood/app/src/main/java/com/food/abstractfood/SignupActivity.java
@@ -1,24 +1,37 @@
 package com.food.abstractfood;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.method.PasswordTransformationMethod;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class SignupActivity extends AppCompatActivity {
     EditText passwordF;
     EditText verifyPasswordF;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        passwordF=(EditText) findViewById(R.id.passwordtxt);
+        passwordF = (EditText) findViewById(R.id.passwordtxt);
+        passwordF.setTransformationMethod(PasswordTransformationMethod.getInstance());
 
-        passwordF.setText("Password", TextView.BufferType.EDITABLE);
+
     }
+
 
 
 }
