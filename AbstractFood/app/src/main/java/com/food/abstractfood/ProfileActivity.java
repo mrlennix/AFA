a06636d79a8a1b6e51cca7c0243ec7dc937061b9
mistereabate <mistretef@yahoo.com>
2016-04-15 22:09:36 -0500

    Profile almost done

--- AbstractFood/app/src/main/java/com/food/abstractfood/ProfileActivity.java
+++ AbstractFood/app/src/main/java/com/food/abstractfood/ProfileActivity.java
@@ -1,13 +1,29 @@
 package com.food.abstractfood;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity{
+
+    ViewPager viewPager;
+    ViewPager viewPager2;
+    SwipeAdapterMyUploads swipeAdapterMyUploads;
+    SwipeAdapterMyFavorites swipeAdapterMyFavorites;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        viewPager2=(ViewPager) findViewById(R.id.view_pager2);
+        swipeAdapterMyUploads = new SwipeAdapterMyUploads(this);
+        swipeAdapterMyFavorites= new SwipeAdapterMyFavorites(this);
+        viewPager.setAdapter(swipeAdapterMyUploads);
+        viewPager2.setAdapter(swipeAdapterMyFavorites);
+
+
     }
 }
