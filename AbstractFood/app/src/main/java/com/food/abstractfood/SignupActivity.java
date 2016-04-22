1118959c34091635d11b00c11cde1e124386253c
mistereabate <mistretef@yahoo.com>
2016-04-22 10:46:48 -0500

    made changes to Sign up

--- AbstractFood/app/src/main/java/com/food/abstractfood/SignupActivity.java
+++ AbstractFood/app/src/main/java/com/food/abstractfood/SignupActivity.java
@@ -3,10 +3,12 @@ package com.food.abstractfood;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class SignupActivity extends AppCompatActivity {
     EditText passwordF;
     EditText verifyPasswordF;
+
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
