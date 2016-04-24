3ecc8bdbe392479a6b77f82ccc343fa0055b6bfd
John Doe <lashrois@yahoo.fr>
2016-04-24 11:06:52 -0500

    made the buttons clickable in homepage, added the foodpage, with list view, step view is still pending.

---
+++ AbstractFood/app/src/main/java/com/food/abstractfood/FoodListViewActivity.java
@@ -0,0 +1,34 @@
+package com.food.abstractfood;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.food.abstractfood.R;
+
+/**
+ * Created by John Doe on 4/22/2016.
+ */
+public class FoodListViewActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foodlistviewz);
+        populateListview();
+
+    }
+
+    private void populateListview() {
+
+        String[] myItems = {"step 1", "step 2", "step 3", "step 4", "step 5", "step 6", "step 7", "step 8", "step 9", "step 10"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.da_item, myItems);
+
+        ListView list = (ListView) findViewById(R.id.Food_listView);
+
+        list.setAdapter(adapter);
+    }
+
+}
--- AbstractFood/app/src/main/java/com/food/abstractfood/Home.java
+++ AbstractFood/app/src/main/java/com/food/abstractfood/Home.java
@@ -12,35 +12,35 @@ public class Home extends AppCompatActivity {
 
 
 
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-
-        ImageButton discoverbutton = (ImageButton) findViewById(R.id.discoverbutton);
-        discoverbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), DiscoverActivity.class);
-
-
-
-
-            }
-        });
-
-
-        ImageButton uploadbutton = (ImageButton) findViewById(R.id.uploadbutton);
-        uploadbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View vws) {
-
-                Intent intent = new Intent(vws.getContext(), UploadActivity.class);
-
-            }
-        });
     }
 
-}
+         public void redirectDiscover(View view)
+            {
+                Intent intent = new Intent(view.getContext(), DiscoverActivity.class);
+                startActivity(intent);
+
+
+
+
+            }
+
+
+
+
+            public void redirectUpload(View vws)
+            {
+
+                Intent intent = new Intent(vws.getContext(), UploadActivity.class);
+                startActivity(intent);
+
+            }
+
+    }
+
+
 
 
