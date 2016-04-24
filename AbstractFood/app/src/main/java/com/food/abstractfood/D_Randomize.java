59098069dcceb7bd3a35ea21d597eed33af4b9af
alexlimon <alex.limon@mavs.uta.edu>
2016-04-24 09:27:19 -0500

    changed some designs, added some necessary class

---
+++ AbstractFood/app/src/main/java/com/food/abstractfood/D_Randomize.java
@@ -0,0 +1,27 @@
+package com.food.abstractfood;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+import com.firebase.client.Firebase;
+import java.io.InterruptedIOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class D_Randomize extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+    }
+}
