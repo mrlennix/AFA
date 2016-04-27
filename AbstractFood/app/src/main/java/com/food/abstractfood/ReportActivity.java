639565e81442f6c725773f374df714e0a7ab6373
lacherois <lashrois@yahoo.fr>
2016-04-27 15:34:31 -0500

    fixed report page

--- AbstractFood/app/src/main/java/com/food/abstractfood/ReportActivity.java
+++ AbstractFood/app/src/main/java/com/food/abstractfood/ReportActivity.java
@@ -1,15 +1,19 @@
 package com.food.abstractfood;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by Lacherois on 4/21/2016.
  */
 public class ReportActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +21,24 @@ public class ReportActivity extends AppCompatActivity {
         setContentView(R.layout.activity_report);
 
 
+        Button cancelBtn = (Button) findViewById(R.id.cancelBtn);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), FoodActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
 
 
+        Button submitBtn = (Button) findViewById(R.id.submitBtn);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Your report was sent to the admin for further consideration", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
 
     }
-
-    private Button cancelBtn = (Button)findViewById(R.id.cancelBtn);
-
-
-
 }
